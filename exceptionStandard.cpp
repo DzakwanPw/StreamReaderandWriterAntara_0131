36195fc3c9e7e84efa0981665cf9e4e5622577a1
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2026-08-18 10:08:27 +0700

    menambahkan exception jika terjadi error didalam try

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -13,4 +13,6 @@ int main()
         //memanggil array element ke-5 yang berarti akan terjadi error
         cout << data.at(3) << endl;
     }
+    //kondisi jika terjadi error didalam try
+    catch (exception& e)
 }
