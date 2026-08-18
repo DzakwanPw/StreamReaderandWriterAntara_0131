628880bb827085b386c3ef76b3275afb890ccf09
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2026-08-18 10:13:29 +0700

    membuat program utama dan menambahkan variabel baris dan input untuk nengkap nama file

--- iowithpath.cpp
+++ iowithpath.cpp
@@ -2,3 +2,15 @@
 #include <fstream>
 #include <string>
 using namespace std;
+
+int main()
+{
+    //deklarasi variabel baris dan Nama File
+    string baris, NamaFile;
+
+    //Membuat input untuk menangkap nama file
+    cout << "Masukkan nama file : " ;
+    cin >> NamaFile;
+
+    
+}
